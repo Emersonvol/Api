9a18033de777afdd7ce92337210a3d43c9cbb17f
Emerson Santos <89021451+Emersonvol@users.noreply.github.com>
2025-05-17 02:41:53 -0300

    aa

--- src/pages/api/ApiMusicInfo.js
+++ src/pages/api/ApiMusicInfo.js
@@ -1,13 +1,17 @@
-function ApiMusicInfo(req, res) {
-    const dataDinamica = new Date();
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+export default function handler(req, res) {
+  // Adiciona CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-    res.json({
-        date: dataDinamica.toGMTString()
-    })
+  if (req.method === "OPTIONS") {
+    res.status(200).end(); // pré-verificação
+    return;
+  }
 
+  const dataDinamica = new Date();
+
+  res.status(200).json({
+    date: dataDinamica.toGMTString(),
+  });
 }
-
-export default ApiMusicInfo;
