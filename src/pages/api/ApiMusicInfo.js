dd91ba3014bf9f4ad374d998d2babed28bdadebc
Emerson Santos <89021451+Emersonvol@users.noreply.github.com>
2025-05-17 02:40:52 -0300

    att

--- src/pages/api/ApiMusicInfo.js
+++ src/pages/api/ApiMusicInfo.js
@@ -1,5 +1,8 @@
 function ApiMusicInfo(req, res) {
     const dataDinamica = new Date();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
     res.json({
         date: dataDinamica.toGMTString()
