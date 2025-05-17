04b1616f5eac44032dc392771437a5cd3ca8262e
Emerson Santos <89021451+Emersonvol@users.noreply.github.com>
2025-05-17 20:58:16 -0300

    a

--- src/pages/api/ApiMusicInfo.js
+++ src/pages/api/ApiMusicInfo.js
@@ -1,20 +1,25 @@
 function ApiMusicInfo(req, res) {
-    const dataDinamica = new Date();
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  const dataDinamica = new Date();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  res.setHeader("https://accounts.spotify.com/api/token")
+  res.setHeader("Content-Type: application/x-www-form-urlencoded")
+  res.setHeader("grant_type=client_credentials&client_id=your-client-id&client_secret=your-client-secret")
+
   if (req.method === "OPTIONS") {
-    res.status(200).end(); 
+    res.status(200).end();
   }
-    res.json({
-        date: dataDinamica.toGMTString(),
-        name:"ola"
-    })
- res.status(200).json({
+  res.json({
+
+
+  })
+  res.status(200).json({
     date: dataDinamica.toGMTString(),
   });
 
-  
+
 }
 
 export default ApiMusicInfo;
