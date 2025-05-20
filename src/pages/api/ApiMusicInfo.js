5bdac303de1345b24fe012a686185e5662cc77b5
Emerson Santos <89021451+Emersonvol@users.noreply.github.com>
2025-05-20 20:56:14 -0300

    att spotify api

--- src/pages/api/ApiMusicInfo.js
+++ src/pages/api/ApiMusicInfo.js
@@ -1,24 +1,38 @@
 async function ApiMusicInfo(req, res) {
-  const dataDinamica = new Date();
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-  const apiDoSpotify = await fetch("https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy") 
-  const Dados = await apiDoSpotify.json()
-  console.log(dados)
-
-
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-  }
-  res.json({
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    
+    const dataDinamica = new Date();
+    res.json({
+        date: dataDinamica.toGMTString(),
+        name: "ola"
 
 
-  })
-  res.status(200).json({
-    date: dataDinamica.toGMTString(),
-  });
+
+    })
+
+
+
+    const clientId = '388f9b1eab034fa0a2071d435f0e96a7'
+    const clientSecret = '5ba16c69a08445479095d220497b7608'
+    const redirectUrl = "https://open.spotify.com/intl-pt"
+
+
+
+    const tokenApi = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Basic ${Buffer.from(clientId + ':' + clientSecret).toString('base64')}`
+        },
+        body: 'grant_type=client_credentials'
+
+    })
+
+    const data = await tokenApi.json();
+    return data.access_token;
+
 
 
 }
