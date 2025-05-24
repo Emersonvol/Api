d5b61af9136dc784f992be963bc7dd0f1e690aa7
Emerson Santos <89021451+Emersonvol@users.noreply.github.com>
2025-05-24 18:04:56 -0300

    att

--- src/pages/api/ApiMusicInfo.js
+++ src/pages/api/ApiMusicInfo.js
@@ -2,10 +2,12 @@ async function ApiMusicInfo(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
+ 
+  const musicas = fetch('/music/musicas.json')
+  const dados = await musicas.json()
   
   res.json({
-    name: "ola"
+    name: `${dados}`
 
   })
 
